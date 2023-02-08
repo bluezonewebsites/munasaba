9bc1f2f87681875d3d1cb8fd3d47db9c5eef1f38
Mohamed <mo.esmael2019@gmail.com>
2023-02-09 00:14:10 +0200

    f9-2

--- app/Models/Prod.php
+++ app/Models/Prod.php
@@ -35,20 +35,85 @@ class Prod extends Model
         'prod_size',
         'img',
     ];
+    public function toArray()
+    {
+        $data["id"] = $this->id;
+        $data["cat_id"]=$this->cat_id;
+        $data["sub_cat_id"]=$this->sub_cat_id;
+        $data["uid"]=$this->uid;
+        $data["name"]=$this->name;
+        $data["price"]=$this->price;
+        $data["created_at"]=$this->created_at;
+        $data["loc"]=$this->loc;
+        $data["country_id"]=$this->country_id;
+        $data["city_id"]=$this->city_id;
+        $data["region_id"]=$this->region_id;
+        $data["lat"]=$this->lat;
+        $data["lng"]=$this->lng;
+        $data["descr"]=$this-> descr;
+        $data["phone"]=$this-> phone;
+        $data["wts"]=$this-> wts;
+        $data["has_chat"]=$this->has_chat;
+        $data["has_wts"]=$this->has_wts;
+        $data["has_phone"]=$this->has_phone;
+        $data["amount"]=$this->amount;
+        $data["tajeer_or_sell"]=$this->tajeer_or_sell;
+        $data["views"]=$this->views;
+        $data["calls"]=$this->calls;
+        $data["errors"]=$this->errors;
+        $data["duration_use_name"]=$this->duration_use_name;
+        $data["duration_use"]=$this->duration_use;
+        $data["sell_cost"]=$this->sell_cost;
+        $data["end_date"]=$this-> end_date;
+        $data["brand_id"]=$this->brand_id;
+        $data["material_id"]=$this->material_id;
+        $data["color"]=$this-> color;
+        $data["color_name"]=$this-> color_name;
+        $data["prod_size"]=$this-> prod_size;
+        $data["img"]=$this->img;
+        $data["deleted"]=$this-> deleted;
+        $data["updated_at"]=$this->updated_at;
+        $data["deleted_at"]=$this-> deleted_at;
+        $data["main_cat_name"]=$this->category ->name_ar;
+        $data["sub_cat_name"]=$this->subCategory ->name_ar;
+        $img=$this->firstprodImage->first();
+       $data["prods_image"] = $img? $img->img: '';
+       $data["mtype"] =  $img? $img->mtype: '';
+       $data["user_name"] = $this->user->name??'';
+       $data["user_last_name"] = $this->user->last_name??'';
+       $data["user_verified"] = $this->user->verified??'';
+       $data["countries_name_ar"] = $this->country->name_ar??'';
+       $data["countries_name_en"] = $this->country->name_en??'';
+       $data["countries_currency_ar"] = $this->country->currency_ar??'';
+       $data["countries_currency_en"] = $this->country->currency_en??'';
+       $data["cities_name_ar"] = $this->city->name_ar??'';
+       $data["cities_name_en"] = $this->city->name_en??'';
+       $data["regions_name_ar"] = $this->region->name_ar??'';
+       $data["regions_name_en"] = $this->region->name_en??'';
+       $data["comments"] = $this-> prodRate->count();
+//        $data["is_block"] = $this->is_block();
+        return $data;
+    }
     protected $appends=[
         'image',
     ];
     public function getImageAttribute()
     {
-        return asset('image/' . $this->img);
+        if ($this->img != null || $this->img != ' '){
+            return asset('image/' . $this->img);
+        }else{
+           $img= $this-> prodImage ->first();
+            return $img ?$img->img : null ;
+        }
+
     }
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class,'cat_id','id');
     }
     public function subCategory()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class,'sub_cat_id','id');
     }
     public function user()
     {
@@ -56,19 +121,30 @@ class Prod extends Model
     }
     public function country()
     {
-        return $this->belongsTo(Country::class);
+        return $this->belongsTo(Country::class)
+            ->withDefault(['id'=>0,
+                'name_ar'=>'',
+                'name_en'=>'',
+                'currency_ar'=>'',
+                'currency_en'=>'',
+            ]);
     }
     public function city()
     {
-        return $this->belongsTo(City::class);
+        return $this->belongsTo(City::class)->withDefault(['id'=>0,'name_ar'=>'','name_en'=>'']);
     }
     public function region()
     {
-        return $this->belongsTo(Region::class);
+        return $this->belongsTo(Region::class)->withDefault(['id'=>0,'name_ar'=>'','name_en'=>'']);
     }
     public function prodImage()
     {
         return $this->hasMany(ProdImage::class);
+    }
+
+    public function firstprodImage()
+    {
+        return $this->hasMany(ProdImage::class)->select('img','mtype');
     }
     public function prodRate()
     {
@@ -78,6 +154,6 @@ class Prod extends Model
     {
         return $this->hasMany(CommentOnProd::class);
     }
-    
+
 
 }
